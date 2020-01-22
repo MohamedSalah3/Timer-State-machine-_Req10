b5d3f5d0c4b3be0f7790d019c237d232b7d371b7
MohamedSalah3 <mhmd.ahmd789@gmail.com>
2020-01-22 21:12:54 +0200

    State machine

---
+++ TimersAndInterrupts/Debug/Interrupts.d
@@ -0,0 +1,12 @@
+Interrupts.d Interrupts.o: .././Interrupts.c .././Interrupts.h \
+ .././registers.h .././std_types.h .././timers.h .././gpio.h
+
+.././Interrupts.h:
+
+.././registers.h:
+
+.././std_types.h:
+
+.././timers.h:
+
+.././gpio.h:
---
+++ TimersAndInterrupts/Debug/timer.d
@@ -0,0 +1,16 @@
+timer.d timer.o: .././timer.c .././led.h .././gpio.h .././std_types.h \
+ .././ledConfig.h .././timers.h .././registers.h .././Interrupts.h
+
+.././led.h:
+
+.././gpio.h:
+
+.././std_types.h:
+
+.././ledConfig.h:
+
+.././timers.h:
+
+.././registers.h:
+
+.././Interrupts.h:
